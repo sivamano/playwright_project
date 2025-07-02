871d879c4229bd8ec1651b752456a609167203fa
Sivaraman Manoharan <menrasi@gmail.com>
2025-07-03 05:17:54 +0530

    Removing examples folder from tracking

--- tests-examples/demoTests/firstTest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
- test('basic test', async ({page})=>{
- await page.goto('https://google.com');
- await expect(page).toHaveTitle('Google');
-});
-
--- tests-examples/demoTests/hello.spec.ts
+++
@@ -1,6 +0,0 @@
-exports.hello = function f1(){
-    return "hello";
-}
-exports.world = function f2(){
-    return "world";
-}
